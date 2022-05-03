f68de4fbbf222f71d5d3a7e0788076529efd120c
testuser-aqil <testuser-aqil@protonmail.com>
2022-05-03 16:09:35 +0400

    Create Footer.astro and ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{html,js,astro}"],
   theme: {
+    fontFamily: {
+      mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      dark0: "#1e2022",
+      dark1: "#303236",
+      dark3: "#3d3f43",
+      "light0": "#ffffff",
+      "light1": "#f0f0f0",
+      "light2": "#6c8c9b"
+    },
+
     extend: {},
   },
   plugins: [],
-}
+};
