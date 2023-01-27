3217ef17113153bca7fa9d63c1a7bcc9e47ed4c4
Aqil <aqilll083@gmail.com>
2023-01-27 15:07:10 +0400

    feat(src): add style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import './style.css';
+</script>
+
+<svelte:head>
+	<title>test2user-aqil</title>
+</svelte:head>
+
+<div class="container">
+	<img src="/avatar1024.png" alt="Avatar" class="avatar" />
+
+	<h1>test2user-aqil</h1>
+</div>
+
+<style>
+	.container {
+		height: 85vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 2rem;
+	}
+	.avatar {
+		height: 200px;
+		width: 200px;
+		border-radius: 100%;
+		filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+	}
+</style>
