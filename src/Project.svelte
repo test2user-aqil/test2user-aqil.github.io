7783bd63b40ac61d6e4e5c114243b80c3d0ee496
Aqil <aqilll083@gmail.com>
2023-01-28 18:44:13 +0400

    chore(src): update `project` component Improve component style

--- src/Project.svelte
+++ src/Project.svelte
@@ -4,28 +4,33 @@
 	export let link: string;
 </script>
 
-<div>
-	<h2>
-		<a href={link} target="_blank" rel="noreferrer">
-			{title}
-		</a>
-	</h2>
+<a href={link} target="_blank" rel="noreferrer">
+	<h2>{title}</h2>
 	<p>{descripton}</p>
-</div>
+</a>
 
 <style>
-	div {
+	a {
 		background: #16171f;
 		border: 2px solid rgb(34, 37, 48);
 		border-radius: 1rem;
 		padding: 2rem 5rem;
 		width: 20rem;
+		transition: 0.2s ease-in-out;
+		text-decoration: none;
 	}
-	h2,
-	p {
+	a:hover {
+		border: 2px solid rgb(51, 54, 68);
+	}
+
+	p,
+	h2 {
 		font-family: 'Roboto Mono', monospace;
 	}
-	p {
+	h2 {
+		text-decoration: underline;
+	}
+	a p {
 		margin: 1rem 0 0 0;
 	}
 </style>
